7118908980ec6a639745e6bdcf2d324cec74a9d7
Rahmatul Ramadhani <rahmatulramadhani06@gmail.com>
2024-12-30 09:49:10 +0700

    setting swagger

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors()
   const configService = app.get(ConfigService)
+  const port = configService.get<string>("port")
+  const baseUrl = configService.get<string>("baseUrl")
+  const prefix = "api/v1"
 
-  await app.listen(configService.get<string>("port"));
+  
+  const swaggerConfig = new DocumentBuilder()
+  .setTitle("Hospital API Services")
+  .setDescription("Hospital API server endpoint")
+  .setVersion("1.0")
+  .addServer(`${baseUrl}/${prefix}`, "Development Server")
+  .build()
+  const documentFactory =  SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup("docs", app, documentFactory)
+  
+  app.enableCors();
+  app.setGlobalPrefix(prefix)
+  await app.listen(port);
 }
 bootstrap();
